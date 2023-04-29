4b1f75f144b9888da73949e0d1991e7041e1c081
taylour <jayluvsdivn@gmail.com>
2023-04-28 18:47:45 -0700

    Bug fix and remove unnecessary try exept block

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -30,41 +30,38 @@ while playing:
     comp_choice = random.randint(0, 2)
     comp_play = options[comp_choice]
 
-    # serialize user input with try except block
-    try:
-        print("Please choose a guess; rock, paper, or scissors all lowercase")
-        print("The computer will choose randomly, it will not cheat")
-        player_play = input()
+    print("Please choose a guess; rock, paper, or scissors all lowercase")
+    print("The computer will choose randomly, it will not cheat")
+    player_play = input()
 
-        if player_play == comp_play:
-            winner = "It's a tie!"
-            Winner(winner, player_play, comp_play)
-            playing = play_again(playing)
-        elif player_play == "rock" and comp_play == "paper":
-            winner = "computer"
-            Winner(winner, player_play, comp_play)
-            playing = play_again(playing)
-        elif player_play == "rock" and comp_play == "scissors":
-            winner = "player"
-            Winner(winner, player_play, comp_play)
-            playing = play_again(playing)
-        elif player_play == "paper" and comp_play == "scissors":
-            winner = "computer"
-            Winner(winner, player_play, comp_play)
-            playing = play_again(playing)
-        elif player_play == "paper" and comp_play == "rock":
-            winner = "player"
-            Winner(winner, player_play, comp_play)
-            playing = play_again(playing)
-        elif player_play == "scissors" and comp_play == "rock":
-            winner = "computer"
-            Winner(winner, player_play, comp_play)
-            playing = play_again(playing)
-        else:
-            winner = "player"
-            Winner(winner, player_play, comp_play)
-            playing = play_again(playing)
-
-    except:
-        print("please only type rock, paper, or scissors in all lowercase")
+    if player_play == comp_play:
+        winner = "It's a tie!"
+        Winner(winner, player_play, comp_play)
+        playing = play_again(playing)
+    elif player_play == "rock" and comp_play == "paper":
+        winner = "computer"
+        Winner(winner, player_play, comp_play)
+        playing = play_again(playing)
+    elif player_play == "rock" and comp_play == "scissors":
+        winner = "player"
+        Winner(winner, player_play, comp_play)
+        playing = play_again(playing)
+    elif player_play == "paper" and comp_play == "scissors":
+        winner = "computer"
+        Winner(winner, player_play, comp_play)
+        playing = play_again(playing)
+    elif player_play == "paper" and comp_play == "rock":
+        winner = "player"
+        Winner(winner, player_play, comp_play)
+        playing = play_again(playing)
+    elif player_play == "scissors" and comp_play == "rock":
+        winner = "computer"
+        Winner(winner, player_play, comp_play)
+        playing = play_again(playing)
+    elif player_play == "scissors" and comp_play == "paper":
+        winner = "player"
+        Winner(winner, player_play, comp_play)
+        playing = play_again(playing)
+    else:
+        print("Please only type rock, paper, or scissors in all lowercase")
         continue
